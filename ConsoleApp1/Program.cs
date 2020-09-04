298e11d23436436c1504f2ffc32839bb5b116c59
student <student@CT42906>
2020-09-04 18:23:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace Kuksenkov_1
+{
+  class Program
+  {
+
+    static void Main(string[] args)
+    {
+      private static void StarWars()
+      {
+        Console.Beep(300, 500);
+        Thread.Sleep(50);
+        Console.Beep(300, 500);
+        Thread.Sleep(50);
+        Console.Beep(300, 500);
+        Thread.Sleep(50);
+        Console.Beep(250, 500);
+        Thread.Sleep(50);
+        Console.Beep(350, 250);
+        Console.Beep(300, 500);
+        Thread.Sleep(50);
+        Console.Beep(250, 500);
+        Thread.Sleep(50);
+        Console.Beep(350, 250);
+        Console.Beep(300, 500);
+        Thread.Sleep(50);
+
+      }
+  }
+}
